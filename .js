2fc207d5110d8b572f2bc969ce94a1e017b77f14
Snehal Nair <snehal_nair@moderncoe.edu.in>
2022-05-07 22:16:55 +0530

    Create .js

---
+++ .js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#input").addEventListener("keydown", function(e) {
+    if (e.code === "Enter") {
+        console.log("You pressed the enter button!")
+    }
+  });
+});
